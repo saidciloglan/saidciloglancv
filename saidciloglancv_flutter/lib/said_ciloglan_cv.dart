12bcebf4a4eff22d5c7b858bc066b19d62b542c4
saidciloglan <saidciloglan@gmail.com>
2019-10-31 05:18:55 +0300

    Yemek molası

--- saidciloglancv_flutter/lib/said_ciloglan_cv.dart
+++ saidciloglancv_flutter/lib/said_ciloglan_cv.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:saidciloglancv_flutter/responsive_widget.dart';
 
 class SaidCiloglanCV extends StatelessWidget {
+  List<Widget> navButtons() => [
+        NavButton(
+          text: "about",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "work",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "contact",
+          onPressed: () {},
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveWidget(
@@ -11,7 +26,7 @@ class SaidCiloglanCV extends StatelessWidget {
             ? Drawer(
                 child: ListView(
                   padding: const EdgeInsets.all(20),
-                  children: <Widget>[],
+                  children: navButtons(),
                 ),
               )
             : null,
@@ -19,8 +34,171 @@ class SaidCiloglanCV extends StatelessWidget {
           child: AnimatedPadding(
             padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
             duration: Duration(seconds: 1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  NavHeader(navButtons: navButtons()),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                  ProfileInfo(),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                  //SocialInfo(),
+                ],
+              ),
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class NavHeader extends StatelessWidget {
+  final List<Widget> navButtons;
+
+  const NavHeader({Key key, this.navButtons}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: navButtons,
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.red})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  profileImage(context) => Container(
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          //backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.deepOrange,
+//            borderRadius: BorderRadius.circular(40),
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+                "https://firebasestorage.googleapis.com/v0/b/flutterfirebasedersleri-c65e1.appspot.com/o/user%2Fsaid?alt=media&token=d0613f40-879a-4e83-a2f9-489a13b713cb"),
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+
+  final profileData = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Hi there! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.orange),
+      ),
+      Text(
+        "Said\nÇiloğlan",
+        textScaleFactor: 5,
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "A Google Developer Expert for Flutter, Dart & Web Tech.\n"
+        "I am also a youtuber having MTechViral youtube channel\n"
+        "where I make tutorials for technology.",
+        softWrap: true,
+        textScaleFactor: 1.5,
+        style: TextStyle(color: Colors.white70),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("Resume"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          OutlineButton(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            shape: StadiumBorder(),
+            child: Text("Say Hi!"),
+            color: Colors.red,
+            onPressed: () {},
+            padding: EdgeInsets.all(10),
+          )
+        ],
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[profileImage(context), profileData],
+      ),
+      smallScreen: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          profileImage(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          profileData
+        ],
       ),
     );
   }
