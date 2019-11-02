80a5243956c177b551dc64986670970f94e393a9
saidciloglan <saidciloglan@gmail.com>
2019-11-02 04:13:51 +0300

    Drawer eklendi

--- saidciloglancv_flutter/lib/said_ciloglan_cv.dart
+++ saidciloglancv_flutter/lib/said_ciloglan_cv.dart
@@ -2,203 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:saidciloglancv_flutter/responsive_widget.dart';
 
 class SaidCiloglanCV extends StatelessWidget {
-  List<Widget> navButtons() => [
-        NavButton(
-          text: "hakkında",
-          onPressed: () {},
-        ),
-        NavButton(
-          text: "iş",
-          onPressed: () {},
-        ),
-        NavButton(
-          text: "iletişim",
-          onPressed: () {},
-        ),
-      ];
-
   @override
   Widget build(BuildContext context) {
     return ResponsiveWidget(
       largeScreen: Scaffold(
-        appBar: AppBar(title: Text("Said Çiloğlan CV")),
-        drawer: ResponsiveWidget.isSmallScreen(context)
-            ? Drawer(
-                child: ListView(
-                  padding: const EdgeInsets.all(20),
-                  children: navButtons(),
+        //appBar: AppBar(title: Text("Said Çiloğlan")),
+        body: SingleChildScrollView(),
+      ),
+      smallScreen: Scaffold(
+        appBar: AppBar(title: Text("Said Çiloğlan")),
+        drawer: SafeArea(
+          child: Drawer(
+            child: ListView(
+              padding: const EdgeInsets.all(20),
+              children: <Widget>[
+                DrawerHeader(
+                  child: Row(
+                    children: <Widget>[
+                      LayoutBuilder(
+                        builder: (context, constraints){
+                          return Container(
+                            width: constraints.maxHeight,
+                            decoration: BoxDecoration(
+                              //backgroundBlendMode: BlendMode.luminosity,
+                              color: Colors.deepOrange,
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: AssetImage("assets/images/pp.jpg"),
+                                alignment: Alignment.center,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Said\nÇiloğlan",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            : null,
-        body: SingleChildScrollView(
-          child: AnimatedPadding(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
-            duration: Duration(seconds: 1),
-            child: ResponsiveWidget(
-              largeScreen: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  NavHeader(navButtons: navButtons()),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                  ProfileInfo(),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-                  //SocialInfo(),
-                ],
-              ),
+                ListTile(
+                  leading: Icon(Icons.person, size: 30,),
+                  title: Text("Who Am I", style: TextStyle(fontSize: 24),),
+                ),
+              ],
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class NavHeader extends StatelessWidget {
-  final List<Widget> navButtons;
-
-  const NavHeader({Key key, this.navButtons}) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return ResponsiveWidget(
-      largeScreen: Row(
-        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
-            ? MainAxisAlignment.center
-            : MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          if (!ResponsiveWidget.isSmallScreen(context))
-            Row(
-              children: navButtons,
-            )
-        ],
-      ),
-    );
-  }
-}
-
-class NavButton extends StatelessWidget {
-  final text;
-  final onPressed;
-  final Color color;
-
-  const NavButton(
-      {Key key,
-      @required this.text,
-      @required this.onPressed,
-      this.color = Colors.red})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlineButton(
-      child: Text(text),
-      borderSide: BorderSide(
-        color: color,
-      ),
-      onPressed: onPressed,
-      highlightedBorderColor: color,
-    );
-  }
-}
-
-class ProfileInfo extends StatelessWidget {
-  profileImage(context) => Container(
-        height: ResponsiveWidget.isSmallScreen(context)
-            ? MediaQuery.of(context).size.height * 0.25
-            : MediaQuery.of(context).size.width * 0.25,
-        width: ResponsiveWidget.isSmallScreen(context)
-            ? MediaQuery.of(context).size.height * 0.25
-            : MediaQuery.of(context).size.width * 0.25,
-        decoration: BoxDecoration(
-          //backgroundBlendMode: BlendMode.luminosity,
-          color: Colors.deepOrange,
-//            borderRadius: BorderRadius.circular(40),
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            image: AssetImage("assets/images/pp.jpg"),
-            alignment: Alignment.center,
-            fit: BoxFit.cover,
-          ),
-        ),
-      );
-
-  final profileData = Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        "Merhaba! Bendeniz",
-        textScaleFactor: 2,
-        style: TextStyle(color: Colors.orange),
-      ),
-      Text(
-        "Said\nÇiloğlan",
-        textScaleFactor: 4,
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      Text(
-        "Gebze Teknik Üniversitesi\n"
-        "Elektronik Mühendisliği\n"
-        "Yapay Zeka, Robotik\n"
-        "Flutter, Dart, Java, Python",
-        softWrap: true,
-        textScaleFactor: 1.5,
-        style: TextStyle(color: Colors.black),
-      ),
-      SizedBox(
-        height: 20,
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          RaisedButton(
-            shape: StadiumBorder(),
-            child: Text("Özgeçmiş"),
-            color: Colors.red,
-            onPressed: () {},
-            padding: EdgeInsets.all(10),
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          OutlineButton(
-            borderSide: BorderSide(
-              color: Colors.red,
-            ),
-            shape: StadiumBorder(),
-            child: Text("Merhaba!"),
-            color: Colors.red,
-            onPressed: () {},
-            padding: EdgeInsets.all(10),
-          )
-        ],
-      )
-    ],
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return ResponsiveWidget(
-      largeScreen: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[profileImage(context), profileData],
-      ),
-      smallScreen: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          profileImage(context),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          profileData
-        ],
+        body: SafeArea(child: SingleChildScrollView()),
       ),
     );
   }
